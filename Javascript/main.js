6b59952f3a32cf5dbfbe4b190eeeab753b039120
Game-Central <101155240+Game-Central@users.noreply.github.com>
2022-05-04 19:51:19 -0700

    Update main.js

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,13 +1,13 @@
-if (document.body.contains(document.getElementById('menuadded'))==true) {
+if (document.getElementById("menuadded")) {
   alert('Menu Already Adedd. If This Is A Mistake, Reload The Page');
 } else{
   //Variables
   var button = document.createElement('img');
-  var body =document.body || document.getElementsByTagName('body');
+  var body = document.body || document.getElementsByTagName('body');
 
   //testing
-  menuaddedtester.id = "menuadded"
   var menuaddedtester = document.createElement('a');
+  menuaddedtester.id = "menuadded"
 
   //Makes The Display For The Menu Button 
   button.src = "https://o.remove.bg/downloads/bd4c7cec-44c9-43c1-bcda-c166ed52690c/menu-hamburger-button-logo-chef-menu-button-removebg-preview.png";
