541480a4ec2353e8c65f992288a477b46756dea9
Game-Central <101155240+Game-Central@users.noreply.github.com>
2022-05-04 19:26:05 -0700

    Create main.js

---
+++ Javascript/main.js
@@ -0,0 +1,33 @@
+var button = document.createElement('img');
+var body =document.body || document.getElementsByTagName('body');
+
+button.src = "https://o.remove.bg/downloads/bd4c7cec-44c9-43c1-bcda-c166ed52690c/menu-hamburger-button-logo-chef-menu-button-removebg-preview.png";
+button.style.width = "80px";
+button.style.width = "80px";
+button.style.draggable = "false";
+document.body.insertBefore(button, body.firstChild);
+
+button.onclick = function() {
+alert('clicked');
+};
+
+button.onmousedown = function(event) {
+  button.style.position = 'absolute';
+  button.style.zIndex = 1000;
+  function moveAt(pageX, pageY) {
+    button.style.left = pageX - button.offsetWidth / 2 + 'px';
+    button.style.top = pageY - button.offsetHeight / 2 + 'px';
+  }
+  moveAt(event.pageX, event.pageY);
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+  document.addEventListener('mousemove', onMouseMove);
+  button.onmouseup = function() {
+    document.removeEventListener('mousemove', onMouseMove);
+    button.onmouseup = null;
+  };
+};
+button.ondragstart = function() {
+  return false;
+};
