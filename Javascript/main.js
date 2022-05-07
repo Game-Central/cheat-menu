43249d91f71ce5bd07373b2c53c952a2c6d1a6d5
Game-Central <101155240+Game-Central@users.noreply.github.com>
2022-05-06 20:28:19 -0700

    Update main.js

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,125 +1,119 @@
-  /* Variables */
+/* Variables */
 
-  var body = document.body || document.getElementsByTagName('body');
-  
+const body = document.body || document.getElementsByTagName('body');
 
-  /* Makes The Menu */
-  
-  var menu = document.createElement("div");
-  menu.style.display = "none";
-  menu.style.width = "150px";
-  menu.style.height = "100%";
-  menu.style.background = "grey";
-  menu.style.position = 'absolute';
-  menu.style.left = "0";
-  menu.style.bottom = "0";
-  menu.style.zIndex = "1000";
-  document.body.insertBefore(menu, body.firstChild);
+/* Makes The Menu */
 
+const menu = document.createElement('div');
+menu.style.display = 'none';
+menu.style.width = '150px';
+menu.style.height = '100%';
+menu.style.background = 'grey';
+menu.style.position = 'absolute';
+menu.style.left = '0';
+menu.style.bottom = '0';
+menu.style.zIndex = '1000';
+document.body.insertBefore(menu, body.firstChild);
 
+/* Makes The Menu Button */
 
-  /* Makes The Menu Button */
+const menuvalue = document.createElement('button');
+const button = document.createElement('img');
+menuvalue.style.display = 'none';
+button.src = 'https://game-central.github.io/cheat-menu/Images/menu-button.png';
+button.style.width = '40px';
+button.style.width = '45px';
+button.style.draggable = 'false';
+button.style.position = 'absolute';
+button.style.right = '0';
+button.style.top = '0';
+button.style.zIndex = '998';
+document.body.insertBefore(button, body.firstChild);
 
-  var menuvalue = document.createElement('button');
-  var button = document.createElement('img');
-  menuvalue.style.display = "none";
-  button.src = "https://game-central.github.io/cheat-menu/Images/menu-button.png";
-  button.style.width = "40px";
-  button.style.width = "45px";
-  button.style.draggable = "false";
+/* Makes The Menu Close Button */
+
+const menuclose = document.createElement('img');
+menuclose.src = 'https://game-central.github.io/cheat-menu/Images/menu-close.png';
+menuclose.style.display = 'none';
+menuclose.style.width = '20px';
+menuclose.style.position = 'absolute';
+menuclose.style.right = '0';
+menuclose.style.top = '0';
+menuclose.style.draggable = 'false';
+menuclose.id = 'menuclose';
+menu.insertBefore(menuclose, menu.firstChild);
+
+/* Menu Drag */
+
+button.onmousedown = function (event) {
   button.style.position = 'absolute';
-  button.style.right = "0";
-  button.style.top = "0";
-  button.style.zIndex = "998";
-  document.body.insertBefore(button, body.firstChild);
-
-
-
-  /* Makes The Menu Close Button */
-
-  var menuclose = document.createElement('img');
-  menuclose.src = "https://game-central.github.io/cheat-menu/Images/menu-close.png";
-  menuclose.style.display = "none";
-  menuclose.style.width = "20px";
-  menuclose.style.position = 'absolute';
-  menuclose.style.right = "0";
-  menuclose.style.top = "0";
-  menuclose.style.draggable = "false";
-  menu.insertBefore(menuclose, menu.firstChild);
-
-
-
-  /* Menu Drag */
-
-  button.onmousedown = function(event) {
-    button.style.position = 'absolute';
-    button.style.zIndex = 100001;
-    function moveAt(pageX, pageY) {
-     button.style.left = pageX - button.offsetWidth / 2 + 'px';
-     button.style.top = pageY - button.offsetHeight / 2 + 'px';
-    }
+  button.style.zIndex = 100001;
+  function moveAt(pageX, pageY) {
+    button.style.left = `${pageX - button.offsetWidth / 2}px`;
+    button.style.top = `${pageY - button.offsetHeight / 2}px`;
+  }
+  moveAt(event.pageX, event.pageY);
+  function onMouseMove(event) {
     moveAt(event.pageX, event.pageY);
-    function onMouseMove(event) {
-     moveAt(event.pageX, event.pageY);
-    }
-    document.addEventListener('mousemove', onMouseMove);
-    button.onmouseup = function() {
-     document.removeEventListener('mousemove', onMouseMove);
-     button.onmouseup = null;
-    };
+  }
+  document.addEventListener('mousemove', onMouseMove);
+  button.onmouseup = function () {
+    document.removeEventListener('mousemove', onMouseMove);
+    button.onmouseup = null;
   };
-  button.ondragstart = function() {
-    return false;
-  };
+};
+button.ondragstart = function () {
+  return false;
+};
 
 button.addEventListener('click', menuOpenClose);
 menuclose.addEventListener('click', menuOpenClose);
 
-function menuOpenClose() {  
-if (menuvalue.value == "1") {
-    menuvalue.value = "0";
+/* Menu Contents */
+
+var menutitle = document.createElement('h3');
+menutitle.innerHTML = 'Cheat Menu 2.0';
+menutitle.style.display = 'none';
+menutitle.style.fontSize = '15px';
+menutitle.style.left = '0';
+menutitle.style.top = '0';
+menutitle.style.draggable = 'false';
+menu.insertBefore(menutitle, menu.firstChild);
+
+var break1 = document.createElement('br');
+menu.insertBefore(break1, menu.lastChild);
+
+var MainPageButtonOne = document.createElement('button');
+MainPageButtonOne.innerHTML = 'test';
+MainPageButtonOne.style.fontSize = '15px';
+MainPageButtonOne.style.width = '100%';
+MainPageButtonOne.style.right = '0';
+MainPageButtonOne.style.top = '0';
+menu.insertBefore(MainPageButtonOne, menu.lastChild);
+
+var MainPageButtonTwo = document.createElement('button');
+MainPageButtonTwo.innerHTML = 'test';
+MainPageButtonTwo.style.fontSize = '15px';
+MainPageButtonTwo.style.width = '100%';
+MainPageButtonTwo.style.right = '0';
+MainPageButtonTwo.style.top = '0';
+menu.insertBefore(MainPageButtonTwo, menu.lastChild);
 
 
-    menu.style.display = "none";
-    menuclose.style.display = "none";
-    menutitle.style.display = "none";
-    MainPageButton1.style.display = "none";
+function menuOpenClose() {
+  if (menuvalue.value === '1') {
+    menuvalue.value = '0';
 
+    menu.style.display = 'none';
+    menuclose.style.display = 'none';
+    menutitle.style.display = 'none';
 
   } else {
-    menuvalue.value = "1";
+    menuvalue.value = '1';
 
-
-    menu.style.display = "initial";
-    menuclose.style.display = "initial";
-    menutitle.style.display = "initial";
-    MainPageButton1.style.display = "initial";
-
+    menu.style.display = 'initial';
+    menuclose.style.display = 'initial';
+    menutitle.style.display = 'initial';
 
   }
-};
-  
-
-
-
-  /* Menu Contents */
-  
-  var menutitle = document.createElement('h3');
-  menutitle.innerHTML = "Cheat Menu 2.0";
-  menutitle.style.display = "none";
-  menutitle.style.fontSize = "15px";
-  menutitle.style.position = 'absolute';
-  menutitle.style.left = "0";
-  menutitle.style.top = "0";
-  menutitle.style.draggable = "false";
-  menu.insertBefore(menutitle, menu.firstChild);
-
-  var MainPageButton1 = document.createElement('button');
-  MainPageButton1.innerHTML = "Cheat Menu 2.0";
-  MainPageButton1.style.display = "none";
-  MainPageButton1.style.width = "20px";
-  MainPageButton1.style.position = 'absolute';
-  MainPageButton1.style.left = "0";
-  MainPageButton1.style.top = "0";
-  MainPageButton1.style.draggable = "false";
-  menu.insertAfter(menutitle, menu.menutitle);
+}
